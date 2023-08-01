ae374b6be2cf92e3ccb7e4f099d596b6db6e7d52
AbdessamadPas <boughadoin@gmail.com>
2023-08-01 01:30:32 +0100

    add custom date picker to form

--- lib/services/movement.dart
+++ lib/services/movement.dart
@@ -11,7 +11,7 @@ class MovementService {
   static Future addMovement(dataToSend) async {
     String serverUrl =
         'http://backup.sendatrack.com/sendatrack-dev-othman/public/api/AddMvt';
-   
+
     String queryString = Uri(queryParameters: dataToSend).query;
 
     String requestUrl = '$serverUrl?$queryString';
@@ -29,4 +29,3 @@ class MovementService {
     }
   }
 }
-u: [{"id":"","id_Store":"2","DateMvt":1690761600,"DateMvtString":"2023-07-31","TypeMvt":"E","NumBon":"","Extra":"","login":"","Qte":1,"Price":350,"Tva":0,"PrixTotal":"","Reference":"ref01","id_TypePanne":0,"id_Vehicule":0,"Kilometrage":0,"observation":"","Designation":"test01","idCiterne":"","id_delivery":0,"QteTotal":"","TVA":0},{"id":"","id_Store":"2","DateMvt":1690761600,"DateMvtString":"","TypeMvt":"E","NumBon":"","Extra":"","login":"","Qte":2,"Price":550,"Tva":0,"PrixTotal":"","Reference":"R2","id_TypePanne":0,"id_Vehicule":0,"Kilometrage":0,"observation":"","Designation":"RX002","idCiterne":"","id_delivery":0,"QteTotal":"","TVA":0}]
--- lib/widgets/dashboardStock/pickerdate.dart
+++ lib/widgets/dashboardStock/pickerdate.dart
@@ -1,36 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:sendatrack/constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_picker_spinner_pop_up/time_picker_spinner_pop_up.dart';
 
-class Pickerdate extends StatelessWidget {
+class Pickerdate extends StatefulWidget {
   const Pickerdate({super.key});
+
+  @override
+  State<Pickerdate> createState() => _PickerdateState();
+}
+
+class _PickerdateState extends State<Pickerdate> {
+  DateTime? selectedDate;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Container(
-      height: 40,
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 245, 245, 245),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "10/10/2021",
-              style: TextStyle(
-                color: kGrayIcon,
-                fontSize: 15,
-                fontWeight: FontWeight.w500,
-              ),
+      child: Container(
+          height: 40,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 245, 245, 245),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 245, 245, 245),
+              borderRadius: BorderRadius.circular(60),
             ),
-            Icon(
-              Icons.calendar_today_outlined,
-              color: Color.fromARGB(255, 165, 165, 165),
-              size: 19,
-            )
-          ]),
-    ));
+            height: 60,
+            child: TimePickerSpinnerPopUp(
+              mode: CupertinoDatePickerMode.date,
+              initTime: DateTime.now(),
+              minTime: DateTime.now().subtract(const Duration(days: 10)),
+              maxTime: DateTime.now().add(const Duration(days: 10)),
+              barrierColor: Color.fromARGB(31, 113, 183, 218),
+              timeFormat: 'dd/MM/yyyy',
+              // Customize your time widget
+              // timeWidgetBuilder: (dateTime) {
+              //   return Text(
+              //     dateTime.day.toString() +
+              //         '/' +
+              //         dateTime.month.toString() +
+              //         '/' +
+              //         dateTime.year.toString(),
+              //     style: const TextStyle(
+              //       color: kGrayIcon,
+              //       fontSize: 15,
+              //       fontWeight: FontWeight.w500,
+              //     ),
+              //   );
+              // },
+              onChange: (dateTime) {
+                setState(() {
+                  selectedDate = dateTime;
+                });
+              },
+            ),
+          )),
+    );
   }
 }
