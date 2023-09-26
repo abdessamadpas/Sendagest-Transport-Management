9d957973777d0cc5a3ebe1a8c877dd87a3c2dfc6
AbdessamadPas <boughadoin@gmail.com>
2023-09-26 19:45:07 +0100

    handle Errors

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import './routers/app_pages.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'middlewares/auth.dart';
+import 'dart:developer';
+import 'package:sendatrack/screens/handleErrors/custom_error_widget.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    log(details.exceptionAsString());
+    runApp(const CustomErrorWidget(
+        errorMessage: "  something went wrong \n please restart the app "));
+  };
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -16,19 +27,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      // needs to be fixed for different screen sizes
+      //! needs to be fixed for different screen sizes and needs to be dynamic depending on the screen size
       designSize: const Size(1100, 1500),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
         return GetMaterialApp(
-          title: "sendaTrack",
+          title: "SendaTrack",
           debugShowCheckedModeBanner: false,
           onGenerateRoute: (settings) {
             // Redirect to home or login based on authentication status
             AuthService.redirectToHomeOrLogin();
             return MaterialPageRoute(
-              builder: (context) => SizedBox.shrink(),
+              builder: (context) => const SizedBox.shrink(),
             );
           },
           initialRoute: AppPages.INITIAL, // Use the constant from AppRoutes
